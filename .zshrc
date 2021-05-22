890edf0d78110c61c2f4dfc2af1cecc75d2885f3
siansiansu <minsiansu@gmail.com>
2021-05-22 20:16:33 +0800

    update setup.sh

--- .zshrc
+++ .zshrc
@@ -77,3 +77,14 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 export GO111MODULE="auto"
+
+
+export LESS_TERMCAP_mb=$'\e[1;32m'
+export LESS_TERMCAP_md=$'\e[1;32m'
+export LESS_TERMCAP_me=$'\e[0m'
+export LESS_TERMCAP_se=$'\e[0m'
+export LESS_TERMCAP_so=$'\e[01;33m'
+export LESS_TERMCAP_ue=$'\e[0m'
+export LESS_TERMCAP_us=$'\e[1;4;31m'
+export PAGER="less"
+
---
+++ setup.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+GN='\033[0;31m'
+YW='\033[0;33m'
+RD='\033[0;32m'
+NC='\033[0;0m'
+
+BREW1=( git zsh tmux vim neovim )
+BREW2=( k9s most bat shellcheck tflint tfenv yamllint nvm )
+CASK=( spotify spectacle kap )
+
+homebrew()
+{
+  if command -v brew > /dev/null;
+  then
+    echo -e "${YW}[WARN]${NC} brew is already installed."
+  else
+    echo -e "${GN}[INFO]${NC} Install Homebrew${i}"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  fi
+}
+
+brewinstall1()
+{
+  for i in "${BREW1[@]}"
+  do
+    if command -v "$i" > /dev/null;
+    then
+      echo -e "${YW}[WARN]${NC} ${i} is already installed."
+    else
+      echo -e "${GN}[INFO]${NC} Install ${i}"
+      brew install "$i"
+    fi
+  done
+}
+
+
+brewinstall2()
+{
+  for i in "${BREW2[@]}"
+  do
+    if command -v "$i" > /dev/null;
+    then
+      echo -e "${YW}[WARN]${NC} ${i} is already installed."
+    else
+      echo -e "${GN}[INFO]${NC} Install ${i}"
+      brew install "$i"
+    fi
+  done
+}
+
+brewcask()
+{
+  for i in "${CASK[@]}"
+  do
+    if command -v "$i" > /dev/null;
+    then
+      echo -e "${YW}[WARN]${NC} ${i} is already installed."
+    else
+      echo -e "${GN}[INFO]${NC} Install ${i}"
+      brew install --cask "$i"
+    fi
+  done
+}
+
+brewupdate()
+{
+  brew update && brew upgrade && brew cleanup
+}
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  homebrew
+
+  #brewupdate
+  brewinstall1
+  #brewinstall2
+  #brewcask
+  #brewupdate
+fi
